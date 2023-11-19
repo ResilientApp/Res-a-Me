966d49c6cae493c4d14cb7b61119bab6bb4339d6
Tsung-Chieh Chen <d12300d@gmail.com>
2023-11-18 16:08:39 -0800

    add AuthGuard to every component

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,7 +1,3 @@
-/**
- * Created by Ryan Balieiro on 08.23.2023
- * Main router.
- */
 import {useData} from "../composables/data.js"
 import RouterView from "../vue/core/RouterView.vue"
 import LoginView from "../vue/core/LoginView.vue"
@@ -9,9 +5,9 @@ import LandingPage from "../vue/core/LandingPage.vue"
 import {createRouter, createWebHistory} from "vue-router"
 
 export function createAppRouter() {
-    const data = useData()
-    const sections = data.getSections()
-    const homeSection = sections[0] || {id: 'home'}
+    const data = useData();
+    const sections = data.getSections();
+    const homeSection = sections[0] || {id: 'home'};
 
     /** Create Home **/
     const routeList = [
@@ -31,25 +27,62 @@ export function createAppRouter() {
         component: RouterView
     }]
 
-    /** Create Section Routes **/
-    for(let i = 1 ; i < sections.length ; i++) {
-        let sectionId = sections[i].id
-
-        routeList.push({
-            path: '/' + sectionId,
-            name: sectionId,
-            component: RouterView
-        })
-    }
-
-    /** Wildcard Route **/
-    routeList.push({
-        path: "/:pathMatch(.*)*",
-        redirect: '/'
-    })
-
-    return createRouter({
+    // Create the router instance
+    const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
         routes: routeList
-    })
+    });
+
+    // Add a global beforeEach guard to validate user is logged in or not
+    router.beforeEach(async (to, from, next) => {
+        let isAuthenticated = false
+
+        try {
+            const response = await fetch("http://127.0.0.1:3033/loadUser", {
+                method: "GET",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                    "Access-Control-Allow-Origin": "*",
+                    "Authorization": `Bearer ` + sessionStorage.getItem('access_token'),
+                }
+            });
+    
+            const json = await response.json();
+
+            if (json.status === 200) {
+                isAuthenticated = true;
+            } else if (json.msg === "Token has expired") {
+                const refreshResponse = await fetch("http://127.0.0.1:3033/refresh", {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8",
+                        "Access-Control-Allow-Origin": "*",
+                        "Authorization": `Bearer ` + sessionStorage.getItem('refresh_token'),
+                    }
+                });
+    
+                const refreshJson = await refreshResponse.json();
+                if (refreshJson.status === 200) {
+                    // update new access token with refresh token
+                    sessionStorage.setItem("access_token", refreshJson['access_token']);
+                    isAuthenticated = true;
+                }
+                else {
+                    throw new Error('Refresh Token Expired!!! Please Log In Again')
+                }
+            }
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+
+        console.log("isAuthenticated = ", isAuthenticated)
+
+        if (to.name === 'login' && isAuthenticated) {
+            next({ name: homeSection['id'] }); // Redirect to home if trying to access login while authenticated
+        } else {
+            next(); // Otherwise, proceed as normal
+        }
+    });
+
+    return router;
 }

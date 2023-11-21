9d9469bca20a11ea29669a724be3614b4d380173
Elliot0122 <107611902+Elliot0122@users.noreply.github.com>
2023-11-20 17:57:18 -0800

    add functions in localdb for search page and add get profile in resdb

--- backend/localdb.py
+++ backend/localdb.py
@@ -4,23 +4,20 @@ def getUserLoginDatabase(email):
     else:
         return loginDatabase[email]
 
-def setUserLoginDatabase(email, password, new_user):
+def setUserLoginDatabase(email, password):
     loginDatabase[email] = {
         "password": password,
-        "user_id": new_user
     } 
 
-def getUserInfoDatabase(user_id):
-    if user_id not in userInfoDatabase:
+def getUserInfoDatabase(email):
+    if email not in userInfoDatabase:
         return False
     else:
-        return userInfoDatabase[user_id]
+        return userInfoDatabase[email]
 
-def setUserInfoDatabase(user_id, email = None, public_key = None, metadata = None):
-    userInfoDatabase[user_id] = {}
-    if email: userInfoDatabase[user_id]["email"] = email
-    if public_key: userInfoDatabase[user_id]["public_key"] = public_key
-    if metadata: userInfoDatabase[user_id]["metadata"] = metadata
+def setUserInfoDatabase(email, public_key = None):
+    userInfoDatabase[email] = {}
+    if public_key: userInfoDatabase[email]["public_key"] = public_key
 
 def getUserKeyPairDatabase(public_key):
     if public_key not in userKeyPairDatabase:
@@ -34,10 +31,20 @@ def setUserKeyPairDatabase(public_key, private_key):
     else:
         userKeyPairDatabase[public_key] = private_key
 
+def getUserListDatabase():
+    return userListDatabase
+
+def setUserListDatabase(email, name, position):
+    new_user = {
+        "email": email,
+        "name": name,
+        "position": position
+    }
+    userListDatabase.append(new_user)
+
 loginDatabase = {
         "jack@gmail.com":{
             "password": "jackjack",
-            "user_id": "user"
         }
     }
 # loginDatabase = {}
@@ -49,17 +56,14 @@ if __name__ == "__main__":
     loginDatabase = {
         "jack@gmail.com":{
             "password": "jackjack",
-            "user_id": "user"
         }
     }
 
     userInfoDatabase = {
-        "user":{
-                "email": "jack@gmail.com",
-                "public_key": "CX62ckRaUucsVi3cbxCkSAcxR7zsBBNSPPrEHz26X4vz",
-                # "fulfilled_tx": "12345",
-                "metadata": {"a": "b"}
-                },
+        "jack@gmail.com":{
+            "public_key": "CX62ckRaUucsVi3cbxCkSAcxR7zsBBNSPPrEHz26X4vz",
+            "transaction_id": "12342356"
+            },
     }
     
     userKeyPairDatabase = {
@@ -68,13 +72,13 @@ if __name__ == "__main__":
 
     userListDatabase = [
         {
+            "email": "jack@gmail.com",
             "name": "Jack",
-            "title": "Software Engineer",
-            "user_id": "user"
+            "position": "Software Engineer"
         },
         {
+            "email": "elliot@gmail.com",
             "name": "Elliot",
-            "title": "Software Engineer",
-            "user_id": "userr"
+            "position": "Software Engineer"
         }
     ]

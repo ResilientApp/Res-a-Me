f0d3839d2b92a4326e5d34b4bf93449732654b5b
Chen-Yu Yu <39978173+tobyyu007@users.noreply.github.com>
2023-12-01 17:10:07 -0800

    Update install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -1,17 +1,6 @@
-cd ./..
-git clone https://github.com/resilientdb/resilientdb.git
-cd ./resilientdb
-./INSTALL.sh
-cd ../Res-a-Me/backend
-git clone https://github.com/ResilientApp/ResilientDB-GraphQL.git
-mv ResilientDB-GraphQL ResilientDB_GraphQL
-
 conda create --name RDB python=3.10
 conda activate RDB
 
 pip install -r requirements.txt
 cd ResilientDB_GraphQL
 pip install -r requirements.txt
-
-bazel build service/http_server:crow_service_main
-cd ..
